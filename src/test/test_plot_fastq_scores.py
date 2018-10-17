44cf7a6b2c1f50d3cd98cb31ecd051053ce719b6
Keenan Graham <keenangraham@stanford.edu>
2018-10-17 16:44:34 -0400

    Test plt called with width and height args

--- src/test/test_plot_fastq_scores.py
+++ src/test/test_plot_fastq_scores.py
@@ -43,3 +43,32 @@ def test_parse_file_name():
     from plot_fastq_scores import parse_file_name
     assert parse_file_name('../../ABC123.fastq.gz') == 'ABC123'
     assert parse_file_name('../../trimmed.ABC123.fastq.gz') == 'ABC123'
+
+
+def test_plot_called_with_width_height(mocker):
+    from plot_fastq_scores import (
+        plot_quality_score_by_position,
+        ReadBase
+    )
+    import matplotlib.pyplot
+    mocker.patch('matplotlib.pyplot.subplots')
+    bin_data = [
+        ReadBase(position=0, quality_score=44),
+        ReadBase(position=1, quality_score=127),
+        ReadBase(position=2, quality_score=900)
+    ]
+    try:
+        plot_quality_score_by_position(
+            bin_data,
+            bin_data,
+            'white',
+            'red',
+            'ticks',
+            100,
+            100,
+            5,
+            3
+        )
+    except Exception as e:
+        pass
+    matplotlib.pyplot.subplots.assert_called_once_with(2, 1, figsize=[5, 3], sharey=True)
